336ee339d08af40732dcfbebba071fcf790b7022
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-05-03 21:29:37 +0200

    Add project files.

---
+++ RandomNumberGame/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace RandomNumberGanme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            var random = new Random();
+            var randomNumber = random.Next(0,100);
+            Console.WriteLine("Udało Ci się wylosować jedną z liczb od 0 do 100. Spróbuj odgadnąć jaka to liczba :)" );
+            var numberOfTries = 0;
+
+            while (true)
+            {
+                numberOfTries++;
+                Console.WriteLine("\nPodaj Twój typ liczby: ");
+                var userNumber = getNumber();
+
+                if (userNumber < randomNumber)
+                {
+                    Console.WriteLine("Twoja liczba jest mniejsza od wylosowanej. Możesz spróbować ponownie.");
+                    continue;
+                }
+                if (userNumber > randomNumber)
+                {
+                    Console.WriteLine("Twoja liczba jest większa od wylosowanej. Możesz spróbować ponownie.");
+                    continue;
+                }
+                Console.WriteLine($"BRAWO!!! Udało Ci się wylosować liczbę w {numberOfTries} próbach.");
+                break;
+            }
+        }
+
+        private static int getNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłową liczbę. Możesz spróbować ponownie.\n");
+                    continue;
+                 
+                }
+                return number;
+            }
+        }
+    }
+}
+
